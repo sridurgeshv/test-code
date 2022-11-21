fcc1a4e837ee949db1bb5a538ab96eee8429ab41
sridurgeshv <60750509+sridurgeshv@users.noreply.github.com>
2022-11-21 16:57:00 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,35 @@
+for i in range(256):
+    l=list(map(int,input().split(" ")))
+    matrix.append(l)
+tl=[]
+tr=[]
+bl=[]
+br=[]
+for i in range(256):
+    for j in range(256):
+        if matrix[i][j]==0:
+            if len(tl)==0:
+                tl.append(i)
+                tl.append(j)
+            else:
+                tl[0]=min(i,tl[0])
+                tl[1]=min(j,tl[1])
+            if len(tr)==0:
+                tr.append(i)
+                tr.append(j)
+            else:
+                tr[0]=min(i,tr[0])
+                tr[1]=max(j,tr[1])
+            if len(bl)==0:
+                bl.append(i)
+                bl.append(j)
+            else:
+                bl[0]=max(i,bl[0])
+                bl[1]=min(j,bl[1])
+            if len(br)==0:
+                br.append(i)
+                br.append(j)
+            else:
+                br[0]=max(i,br[0])
+                br[1]=max(j,br[1])
+print(tuple(tl),tuple(tr),tuple(bl),tuple(br))
